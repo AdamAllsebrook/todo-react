c2bae0b96dcae8df1ab0815bf10ca8e4a7e58e9e
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-10-22 11:13:50 +0100

    Add icons

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AuthLink from './AuthLink';
+import { CheckSquare } from 'lucide-react';
 
 export default async function Nav() {
     return (
         <nav className="flex w-full justify-between border-b p-2">
-            <h1 className="text-2xl my-auto">Todos!</h1>
+            <div className="my-auto flex">
+                <CheckSquare className="w-6 h-6 mr-2 my-auto" />
+                <h1 className="text-2xl">Todos!</h1>
+            </div>
             <AuthLink />
         </nav>
     )
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -16,6 +16,7 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
+import { ArrowDown, ChevronDown, LogOut, User } from "lucide-react"
 
 
 export default function UserDropdown({ name }: { name: string }) {
@@ -23,9 +24,13 @@ export default function UserDropdown({ name }: { name: string }) {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="outline">{name}</Button>
+                <Button variant="outline">
+                    <User className="w-4 h-4 mr-2" />
+                    {name}
+                    <ChevronDown className="w-4 h-4 ml-2" />
+                </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56">
+            <DropdownMenuContent>
                 <DropdownMenuLabel>
                     My Account
                 </DropdownMenuLabel>
@@ -33,7 +38,10 @@ export default function UserDropdown({ name }: { name: string }) {
                 <DropdownMenuGroup>
                     <DropdownMenuItem className="p-0">
                         <form method="POST" action="/auth/sign-out" className="w-full">
-                            <Button type="submit" variant="ghost" className="px-2 py-1.5 w-full justify-start">Sign Out</Button>
+                            <Button type="submit" variant="ghost" className="px-2 py-1.5 w-full justify-start">
+                                <LogOut className="w-4 h-4 mr-2" />
+                                Sign Out
+                            </Button>
                         </form>
                     </DropdownMenuItem>
                 </DropdownMenuGroup>
